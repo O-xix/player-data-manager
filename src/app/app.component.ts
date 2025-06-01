f3e2a002c695dce1d38f43a813992d081cc4696f
Teja Dasari <ichorlifephoenix@gmail.com>
2025-05-31 23:58:01 -0700

    Small package changes and component/page additions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+// For each component, import like so above, and...
+import { AboutPageComponent } from './pages/about-page/about-page/about-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, AboutPageComponent],
+  // ...add the component to the imports array above this line.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full' },
+    { path: 'about', component: AboutPageComponent },
+];
